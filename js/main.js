55f32756725e1431f82021c2bc9dc0e01d032ec3
Vilija Kontrimaite <vikontrimaite@gmail.com>
2020-12-28 15:12:00 +0200

    update secret message style and add functionality

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    secretMsg = document.getElementById('secret-msg');
+    secretMsg.className = 'secret-box';
+
+    secretBtn = document.getElementById('secret-btn');
+    secretBtn.className = 'hidden';
+}
